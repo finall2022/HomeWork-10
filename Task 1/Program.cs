c8b9d4608872d7c0c0727ca6f3c98d745327136a
Aleks Fin <finall.git2022@gmail.com>
2022-07-11 16:37:22 +0300

    ДЗ к 10 му семинару

---
+++ Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 1: Есть монотонная последовательность, 
+каждое число встречается ровно то количество раз, 
+какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... 
+Выведите ряд этой последовательности до N-го значения, 
+желательно использовать рекурсию:
+N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5" */
+Console.WriteLine("Программа вывода монотонной последовательности до до N-го значения");
+Console.WriteLine("Введите значение N: ");
+string inputn = Console.ReadLine();
+int n = int.Parse(inputn);
+int[] arr = new int[n];
+
+int Sequence(int nTemp, int iTemp)
+{
+
+    if (iTemp == nTemp) return nTemp;
+    return Sequence(nTemp - 1, iTemp);
+}
+int i = 1;
+Console.Write(Sequence(n, i));
