12f5702e194d99cec2ed139984e62dfe6c5e9c6e
Aleks Fin <finall.git2022@gmail.com>
2022-07-13 14:42:33 +0300

    ДЗ к 10 му семинару

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,20 +1,23 @@
-﻿/* Задача 1: Есть монотонная последовательность, 
-каждое число встречается ровно то количество раз, 
-какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... 
-Выведите ряд этой последовательности до N-го значения, 
+﻿/* Задача 1: Есть монотонная последовательность,
+каждое число встречается ровно то количество раз,
+какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,...
+Выведите ряд этой последовательности до N-го значения,
 желательно использовать рекурсию:
 N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5" */
 Console.WriteLine("Программа вывода монотонной последовательности до до N-го значения");
 Console.WriteLine("Введите значение N: ");
 string inputn = Console.ReadLine();
 int n = int.Parse(inputn);
-int[] arr = new int[n];
 
-int Sequence(int nTemp, int iTemp)
+void PrintSeq(int n, int k)
 {
+    var numbers = (k + 1) * k / 2;
 
-    if (iTemp == nTemp) return nTemp;
-    return Sequence(nTemp - 1, iTemp);
+    for (int i = numbers - k; i < numbers & i < n; i++)
+        Console.Write($"{k}, ");
+
+    if (numbers < n)
+        PrintSeq(n, k + 1);
 }
-int i = 1;
-Console.Write(Sequence(n, i));
+
+PrintSeq(n, 1);
--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,3 +1,21 @@
-﻿/* Задача 2: Напишите программу вычисления функции Аккермана 
+﻿/* Задача 2: Напишите программу вычисления функции Аккермана
 с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 3, n = 2 -> A(m,n) = 29 */
+Console.Write("Введите число n: ");
+string inputn = Console.ReadLine();
+int n = int.Parse(inputn);
+Console.Write("Введите число m: ");
+string inputm = Console.ReadLine();
+int m = int.Parse(inputm);
+
+int A(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    if (n != 0 && m == 0)
+        return A(n - 1, 1);
+    if (n > 0 && m > 0)
+        return A(n - 1, A(n, m - 1));
+    return A(n, m);
+}
+Console.Write(A(n, m));
